7ac7ecae420b9fc076d9a23680a85848fbc36b3e
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2017-11-23 20:09:17 +1100

    First commit

---
+++ prac_exp_one/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+namespace prac_exp_one
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int results = 50;
+
+            WriteLine("{0}! You scored {1} marks for your test.",
+                      "Good morning", results);
+        }
+    }
+}
